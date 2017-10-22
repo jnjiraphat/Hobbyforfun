fd8bb895e9a0ea129145b945627d0300a3c4dfe9
gnbiiie3 <gnbiiie3@hotmail.com>
2017-10-22 13:46:13 +0700

    ADD

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from './logo.svg';
+
+const Component =()=> (
+  <div>
+    <div className="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+    </div>
+  </div>
+)
+
+export default Component
