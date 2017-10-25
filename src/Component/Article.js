99524728d840e404a7d458b3cf6a90a7f321c062
mild20091931 <mild20091931@gmail.com>
2017-10-26 03:36:10 +0700

    ADD

--- src/Component/Article.js
+++ src/Component/Article.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Article = () => (
-    <div></div>
+    <div className="home-wrap clearfix">
+        < div className="text-center col-lg-12 bghead ">
+            <div className="row">
+                
+            </div>
+        </div>
+    </div>
 )
 export default Article
