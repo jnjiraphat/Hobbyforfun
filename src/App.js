c7d4a7bafe1e71d1ac13a0127661a2c15a985047
mild20091931 <mild20091931@gmail.com>
2017-10-26 01:12:04 +0700

    ADD

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Menubar from './Component/Menubar'
 import PopupForm from './Component/PopupForm'
+import Header from './Component/Header'
+import Logo from './img/pic1.png'
 
-const Component =()=> (
+const Component = () => (
   <div>
-    
-    <Menubar />
-    
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Register</button>
-    <PopupForm />
-    
+    <div className="home-wrap clearfix">
+      < div className="text-center col-lg-12 bghead ">
+        <div className="row">
+          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Register</button>
+          <Header />
+        </div>
+        <PopupForm />
+        <div className="row">
+          < div className="text-center col-lg-12 bgnav ">
+            <Menubar />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
