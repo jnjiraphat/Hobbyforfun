9b4689ba5f46d606f557ce516dba69d5efc557a9
gnbiiie3 <gnbiiie3@hotmail.com>
2017-10-26 03:19:42 +0700

    Topiccard

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Menubar from './Component/Menubar'
 import PopupForm from './Component/PopupForm'
 import Register from './Component/Register'
+import TopicCard from './Component/TopicCard'
 
 const Component =()=> (
   <div>
     
     <Register />
     <PopupForm />
-    <div className="test-box"></div>
     <Menubar />
-    <div className="test-box"></div>
-  </div>
+    {/* <div className="d-flex justify-content-center">
+      <TopicCard message="ABOUT" />
+    </div> */}
+    </div>
 )
 
 export default Component
--- src/Component/Register.js
+++ src/Component/Register.js
@@ -3,7 +3,7 @@ import PopupForm from './PopupForm'
 
 const Register =()=> (
     <div className="container col-12">
-    <div className="circle Cposition text-center" 
+    <div className="circle Cposition text-center " 
         data-toggle="modal" data-target="#myModal">
         <p className="Ctext font-regis">Register NOW!</p>
     </div>
